4fc9a12c51f0c25bb4e2b29f3eed9a667a1e92f0
justmysomnium-cmyk <justmysomnium@gmail.com>
2026-05-16 10:34:11 +0900

    last commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const windowWidth = window.innerWidth;
-const swiperEl = document.querySelector(".swiper");
+const swiperPagination = document.querySelector(".swiper-pagination");
 const mainSection = document.querySelector(".main");
-const mainSectionChildren = mainSection.children;
+const readMoreButton = document.querySelector(".readmore");
+const swiperSlide = Array.from(document.querySelectorAll(".swiper-slide"));
 let mySwiper;
 
 function initSwiper() {
@@ -10,19 +10,48 @@ function initSwiper() {
     if (mySwiper) {
       mySwiper.destroy(true, true);
       mySwiper = null;
+      swiperPagination.style.display = "none";
     }
   } else {
-    mySwiper = new Swiper(".swiper", {
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      spaceBetween: 10,
-      slidesPerView: 1.1,
-    });
+    if (!mySwiper) {
+      mySwiper = new Swiper(".swiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        spaceBetween: 10,
+        slidesPerView: 1.1,
+      });
+      swiperPagination.style.display = "block";
+    }
   }
 }
 
 initSwiper();
 
 window.addEventListener("resize", initSwiper);
+
+function readMoreClick() {
+  if (window.innerWidth >= 768) {
+    readMoreButton.addEventListener("click", () => {
+      swiperSlide.forEach((slide) => {
+        slide.classList.remove("hidden");
+      });
+      if
+      readMoreButton.textContent = "Скрыть";
+    });
+  }
+}
+
+function toggleReadMore() {
+  if (window.innerWidth >= 768) {
+    swiperSlide.forEach((slide, index) => {
+      slide.classList.add("hidden");
+      if (index < 6) {
+        slide.classList.remove("hidden");
+      } 
+    });
+    readMoreClick();
+  }
+}
+toggleReadMore();
